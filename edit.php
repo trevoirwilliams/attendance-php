1b126de3cccdd47767713e766eb2ab6799b5f64e
Trevoir Williams <trevoir.williams@gmail.com>
2019-10-06 04:01:34 +0000

    Update edit.php

--- edit.php
+++ edit.php
@@ -58,7 +58,8 @@
             <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else.</small>
         </div>
         
-        <button type="submit" name="submit" class="btn btn-success btn-block">Save Changes</button>
+        <a href="viewrecords.php" class="btn btn-default">Back To List</a>
+        <button type="submit" name="submit" class="btn btn-success">Save Changes</button>
     </form>
 
 <?php } ?>
