e5f6d6340c89834569d85dd5e8694d87d13cbb4f
Trevoir Williams <trevoir.williams@gmail.com>
2019-10-12 20:57:21 +0000

    added remote mysql connection details

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,16 @@
 <?php 
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
+    // Development Connection
+    // $host = '127.0.0.1';
+    // $db = 'attendance_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset = 'utf8mb4';
+
+    //Remote Database Connection
+    $host = 'remotemysql.com';
+    $db = 'NC1HPsPMvA';
+    $user = 'NC1HPsPMvA';
+    $pass = 'rqUDdNcsXH';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -11,7 +19,7 @@
         $pdo = new PDO($dsn, $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    } catch(PDOException $e) {z
+    } catch(PDOException $e) {
         throw new PDOException($e->getMessage());
     }
 
